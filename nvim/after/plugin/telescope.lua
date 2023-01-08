14cc204313bf40b1c60364ffb976eaf533db6786
Jaskaran Singh <jaskaran110398@gmail.com>
2023-01-09 10:20:05 +1100

    Add more plugins

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,6 +1,4 @@
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>f', builtin.find_files, {})
-vim.keymap.set('n', '<leader>gpf', builtin.git_files, {})
-vim.keymap.set('n', '<leader>grsi', function()
-  builtin.grep_string({ search = vim.fn.input("Grep > ") });
-end)
+-- builtin.setup{  defaults = { file_ignore_patterns = { "node_modules" }} }
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
